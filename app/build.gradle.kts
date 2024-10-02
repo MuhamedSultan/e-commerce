7fe8fdd4d0d8a160b9a1d6e1ddad4e721dad406a
Muhamed Sultan <muhamedsultan211@gmail.com>
2024-10-02 23:23:55 +0300

    Add Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
+    id("androidx.navigation.safeargs")
 }
 
 android {
@@ -33,6 +35,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -45,4 +50,75 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
+    // Retrofit and Gson
+    implementation(libs.retrofit)
+    implementation(libs.retrofit2.converter.gson)
+    implementation(libs.gson)
+
+    // OkHttp
+    implementation(libs.okhttp)
+    implementation(libs.logging.interceptor)
+
+    // Glide
+    implementation(libs.glide)
+
+    // Room
+    implementation(libs.androidx.room.runtime)
+    kapt(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+
+    // Lifecycle components
+    implementation(libs.androidx.lifecycle.extensions)
+    implementation(libs.androidx.lifecycle.common.java8)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+
+    // Navigation
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.ui)
+
+    // Circle Image
+    implementation(libs.circleimageview)
+
+    // Lottie Animation
+    implementation(libs.lottie)
+
+    // Google Play Services - Location
+    implementation(libs.play.services.location)
+
+    // AndroidX Core KTX
+    implementation(libs.androidx.core.ktx.v160)
+
+    // Unit Testing
+    testImplementation(libs.junit)
+    testImplementation(libs.hamcrest.all)
+    testImplementation(libs.androidx.core.testing)
+    testImplementation(libs.robolectric)
+    testImplementation(libs.androidx.junit.ktx)
+    testImplementation(libs.core.ktx)
+
+    // Android Instrumentation Testing
+    androidTestImplementation(libs.androidx.espresso.core.v340)
+    androidTestImplementation(libs.androidx.core.testing)
+
+    // Timber
+    implementation(libs.timber)
+
+    // Hamcrest
+    testImplementation(libs.hamcrest)
+    testImplementation(libs.hamcrest.library)
+    androidTestImplementation(libs.hamcrest)
+    androidTestImplementation(libs.hamcrest.library)
+
+    // Kotlin Coroutines
+    implementation(libs.kotlinx.coroutines.android)
+    testImplementation(libs.jetbrains.kotlinx.coroutines.test)
+    androidTestImplementation(libs.jetbrains.kotlinx.coroutines.test)
+
+    //Google Maps
+    implementation(libs.play.services.maps)
+    implementation(libs.play.services.location)
+
+
 }
