c8005f1f1c39e94d6663c34c0fd13756e888e71e
Muhamed Sultan <muhamedsultan211@gmail.com>
2024-10-02 23:26:26 +0300

    Setup Bottom Navigation View

--- app/src/main/java/com/example/e_commerce_app/MainActivity.kt
+++ app/src/main/java/com/example/e_commerce_app/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.example.e_commerce_app
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.example.e_commerce_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        navController = findNavController(this, R.id.navHostfragment)
+        setupActionBarWithNavController(this, navController)
+        setupWithNavController(binding.bottomNavigationView, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
