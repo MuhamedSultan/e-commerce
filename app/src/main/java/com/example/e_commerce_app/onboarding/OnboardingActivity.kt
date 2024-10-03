f47e872b8364d6e0a24aa2d9ccdf8c130746f6c2
Mohammedhesham <mohammedhe4am@gmail.com>
2024-10-03 23:20:57 +0300

    Handle OnBoarding Screen Ui

--- app/src/main/java/com/example/e_commerce_app/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/example/e_commerce_app/onboarding/OnboardingActivity.kt
@@ -1,21 +1,33 @@
 package com.example.e_commerce_app.onboarding
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.e_commerce_app.R
+import com.example.e_commerce_app.auth.LoginActivity
+import com.example.e_commerce_app.databinding.ActivityOnboardingBinding
 
 class OnboardingActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityOnboardingBinding
+    lateinit var btn_getStart: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_onboarding)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        btn_getStart = binding.btnGetStart
+        btn_getStart.setOnClickListener {
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
+
     }
 }
