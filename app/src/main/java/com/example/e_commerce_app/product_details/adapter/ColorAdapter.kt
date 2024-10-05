62232d55495003b90181e4da944a812194cc7218
Mohammedhesham <mohammedhe4am@gmail.com>
2024-10-05 14:09:16 +0300

    Load the corresponding image for each color

--- app/src/main/java/com/example/e_commerce_app/product_details/adapter/ColorAdapter.kt
+++ app/src/main/java/com/example/e_commerce_app/product_details/adapter/ColorAdapter.kt
@@ -1,17 +1,17 @@
 package com.example.e_commerce_app.product_details.adapter
 
-
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.e_commerce_app.R
 
-class ColorAdapter(private val colors: List<String>) : RecyclerView.Adapter<ColorAdapter.ColorViewHolder>() {
+class ColorAdapter(private val colors: List<String>) :
+    RecyclerView.Adapter<ColorAdapter.ColorViewHolder>() {
 
     class ColorViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val colorTextView: TextView = itemView.findViewById(R.id.color_item)
+        val colorImageView: ImageView = itemView.findViewById(R.id.color_item_image)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ColorViewHolder {
@@ -20,7 +20,19 @@ class ColorAdapter(private val colors: List<String>) : RecyclerView.Adapter<Colo
     }
 
     override fun onBindViewHolder(holder: ColorViewHolder, position: Int) {
-        holder.colorTextView.text = colors[position]
+        val color = colors[position]
+        when (color) {
+            "white" -> holder.colorImageView.setImageResource(R.drawable.white)
+            "black" -> holder.colorImageView.setImageResource(R.drawable.black)
+            "blue" -> holder.colorImageView.setImageResource(R.drawable.blue)
+            "red" -> holder.colorImageView.setImageResource(R.drawable.red)
+            "gray" -> holder.colorImageView.setImageResource(R.drawable.gray)
+            "yellow" -> holder.colorImageView.setImageResource(R.drawable.yellow)
+            "beige" -> holder.colorImageView.setImageResource(R.drawable.beige)
+            "light_brown" -> holder.colorImageView.setImageResource(R.drawable.light_brown)
+            "burgandy" -> holder.colorImageView.setImageResource(R.drawable.burgandy)
+            else -> holder.colorImageView.setImageResource(R.drawable.light_brown)
+        }
     }
 
     override fun getItemCount() = colors.size
