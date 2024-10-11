7218f22403c44fa6d9f99e8f1b1ee352a9cd0d05
mk8084721 <73140794+mk8084721@users.noreply.github.com>
2024-10-11 11:16:16 +0300

    payment api

---
+++ app/src/main/java/com/example/e_commerce_app/payment/PayPalService.kt
@@ -0,0 +1,41 @@
+package com.example.e_commerce_app.payment
+
+
+import retrofit2.http.*
+
+
+
+interface PayPalService {
+
+    // Fetch Access Token
+    @FormUrlEncoded
+    @POST("v1/oauth2/token")
+    suspend fun fetchAccessToken(
+        @Header("Authorization") authorization: String,
+        @Field("grant_type") grantType: String = "client_credentials"
+    ): AccessTokenResponse
+
+    // Create an Order
+    @POST("v2/checkout/orders")
+    suspend fun createOrder(
+        @Header("Authorization") authorization: String,
+        @Header("PayPal-Request-Id") requestId: String,
+        @Body orderRequest: OrderRequest
+    ): OrderResponse
+
+    // Capture an Order
+    @POST("v2/checkout/orders/{order_id}/capture")
+    suspend fun captureOrder(
+        @Header("Authorization") authHeader: String,
+        @Path("order_id") orderId: String,
+        @Body captureRequest: CaptureRequest
+    ): OrderResponse
+}
+///@POST("v2/checkout/orders/{order_id}/capture")
+//suspend fun captureOrder(
+//    @Header("Authorization") authHeader: String,
+//    @Header("PayPal-Partner-Attribution-Id") partnerId: String,
+//    @Header("PayPal-Auth-Assertion") authAssertion: String,
+//    @Path("order_id") orderId: String,
+//    @Body captureRequest: CaptureRequest? = null // Send null or an empty object
+//): OrderResponse
---
+++ app/src/main/java/com/example/e_commerce_app/payment/RetrofitClient.kt
@@ -0,0 +1,20 @@
+package com.example.e_commerce_app.payment
+
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "https://api-m.sandbox.paypal.com/"
+
+    val instance: PayPalService by lazy {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+
+            .addConverterFactory(GsonConverterFactory.create())
+
+            .build()
+
+        retrofit.create(PayPalService::class.java)
+    }
+}
