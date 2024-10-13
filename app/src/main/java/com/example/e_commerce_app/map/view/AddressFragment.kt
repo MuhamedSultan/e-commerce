206babe543f6feddc89a7e9cee6b5c2b69a72095
mk8084721 <73140794+mk8084721@users.noreply.github.com>
2024-10-13 12:58:47 +0300

    edit back button in address fragment

--- app/src/main/java/com/example/e_commerce_app/map/view/AddressFragment.kt
+++ app/src/main/java/com/example/e_commerce_app/map/view/AddressFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
@@ -166,6 +167,13 @@ class AddressFragment : Fragment() {
                         }
                     }
                 }
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            if (page == "setting") {
+                findNavController().navigate(AddressFragmentDirections.actionAddressFragmentToSettingFragment())
+            } else {
+                findNavController().navigate(AddressFragmentDirections.actionAddressFragmentToCartFragment())
             }
         }
     }
